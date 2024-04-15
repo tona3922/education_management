a59400adbc704a9808613cf09f74d765c403e20b
Toan Vo <vothaitoan12@gmail.com>
2024-04-15 22:58:57 +0700

    feat: add reload after update

--- src/pages/Management/Student/components/setGPA.ts
+++ src/pages/Management/Student/components/setGPA.ts
@@ -2,24 +2,24 @@ import { updateDoc, doc, getDoc } from "firebase/firestore";
 import { db } from "../../../../firebase";
 
 export const setGPA = async (studentId: string) => {
-    //console.log(studentId, courseCode)
-    let totalScore = 0;
-    let totalCredit = 0;
-    const docRefUser = doc(db, "users", studentId);
-    const docSnapUser = await getDoc(docRefUser);
-    const listCourses = await docSnapUser.data()?.listCourses;
-    const promises = Object.keys(listCourses).map(async (item) => {
-        const docRefCourse = doc(db, "courses", item);
-        const docSnapCourse = await getDoc(docRefCourse);
-        const credit = await docSnapCourse.data()?.credit;
-        const average = await listCourses[item]?.average
-        totalScore  += credit*average
-        totalCredit += credit*1
-        console.log(item, credit, average)
-    })
-    await Promise.all(promises)
-    const gpa = totalScore/totalCredit
-    await updateDoc(doc(db, `users`, studentId), {
-        GPA : gpa
-    });
-}
+  //console.log(studentId, courseCode)
+  let totalScore = 0;
+  let totalCredit = 0;
+  const docRefUser = doc(db, "users", studentId);
+  const docSnapUser = await getDoc(docRefUser);
+  const listCourses = await docSnapUser.data()?.listCourses;
+  const promises = Object.keys(listCourses).map(async (item) => {
+    const docRefCourse = doc(db, "courses", item);
+    const docSnapCourse = await getDoc(docRefCourse);
+    const credit = await docSnapCourse.data()?.credit;
+    const average = await listCourses[item]?.average;
+    totalScore += credit * average;
+    totalCredit += parseInt(credit);
+    console.log(item, credit, average);
+  });
+  await Promise.all(promises);
+  const gpa = totalScore / totalCredit;
+  await updateDoc(doc(db, `users`, studentId), {
+    GPA: gpa,
+  });
+};
--- src/pages/Management/Student/setGrade.ts
+++ src/pages/Management/Student/setGrade.ts
@@ -2,25 +2,26 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../../../firebase";
 
 export const setGrade = async (
-    midleScore: number,
-    finalScore: number,
-    studentId: string,
-    courseCode: string
+  midleScore: number,
+  finalScore: number,
+  studentId: string,
+  courseCode: string
 ) => {
-    if (
-        midleScore >= 0 &&
-        midleScore <= 10 &&
-        finalScore >= 0 &&
-        finalScore <= 10
-    ) {
-        await updateDoc(doc(db, `users`, studentId), {
-            [`listCourses.${courseCode}`]: {
-                midterm: midleScore,
-                final: finalScore,
-                average: (midleScore + finalScore) / 2,
-            },
-        });
-    } else {
-        alert("Enter the score again");
-    }
+  if (
+    midleScore >= 0 &&
+    midleScore <= 10 &&
+    finalScore >= 0 &&
+    finalScore <= 10
+  ) {
+    await updateDoc(doc(db, `users`, studentId), {
+      [`listCourses.${courseCode}`]: {
+        midterm: midleScore,
+        final: finalScore,
+        average: (midleScore + finalScore) / 2,
+      },
+    });
+    window.location.reload();
+  } else {
+    alert("Enter the score again");
+  }
 };
